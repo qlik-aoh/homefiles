a92597c7cdec55f70d2bb7716e05ae68bc8b8187
Markus Helgesson <Markus.Helgesson@qlik.com>
2018-02-27 16:12:29 +0100

    Fixed bug init

--- homefiles-install.sh
+++ homefiles-install.sh
@@ -10,15 +10,15 @@ function homefiles {
 }
 
 git clone --bare git@github.com:qlik-aoh/homefiles.git $HOME/.homefiles
-homefiles checkout
-mkdir -p .config-backup
+homefiles checkout --quiet
 if [ $? = 0 ]; then
 	echo "Checked out homefiles.";
 else
 	echo "Backing up pre-existing dot files to .config-backup.";
+	mkdir -p .config-backup
 	homefiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 
-homefiles checkout
+homefiles checkout --quiet
 homefiles config status.showUntrackedFiles no
 
