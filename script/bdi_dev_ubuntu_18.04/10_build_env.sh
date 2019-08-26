566562afa568a4d174fd1690cec26cffa2a59906
Markus Helgesson <Markus.Helgesson@qlik.com>
2019-08-26 07:45:38 +0000

    Clang 7

--- script/bdi_dev_ubuntu_18.04/10_build_env.sh
+++ script/bdi_dev_ubuntu_18.04/10_build_env.sh
@@ -10,44 +10,58 @@ fi
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -xeuo pipefail
 
+
 # Make sure we have the latest package list
 apt-get update
 
+# LLVM
+apt-get install -y libllvm-7-ocaml-dev libllvm7 llvm-7 llvm-7-dev llvm-7-doc llvm-7-examples llvm-7-runtime
+# Clang and co
+apt-get install -y clang-7 clang-tools-7 clang-7-doc libclang-common-7-dev libclang-7-dev libclang1-7 clang-format-7 python-clang-7
+# libfuzzer
+apt-get install -y libfuzzer-7-dev
+# lldb
+apt-get install -y lldb-7
+# lld (linker)
+apt-get install -y lld-7
+# libc++
+apt-get install -y libc++-7-dev libc++abi-7-dev
+# OpenMP
+apt-get install -y libomp-7-dev
+# tidy and format
+apt-get install -y clang-format-7 clang-tidy-7
 
-# Install clang 6.0 as default clang
-apt-get install -y clang-6.0 clang-6.0-doc libclang-common-6.0-dev libclang-6.0-dev libclang1-6.0 libclang1-6.0-dbg   \
-	       libllvm6.0 libllvm6.0-dbg lldb-6.0 llvm-6.0 llvm-6.0-dev llvm-6.0-doc llvm-6.0-examples \
-	       llvm-6.0-runtime clang-format-6.0 python-clang-6.0 libfuzzer-6.0-dev clang-tidy-6.0
+# Add to alternatives with high prio and use as default
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-7 150 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-7
+update-alternatives --auto clang
+
+update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-7 150
+update-alternatives --auto clang-tidy
  
-update-alternatives --install /usr/bin/clang clang /usr/bin/clang-6.0 100 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-6.0
-update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-6.0 100
-update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-6.0 100 --slave /usr/bin/git-clang-format git-clang-format /usr/bin/clang-format-6.0
-update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-6.0 100
-
-
-# Install libc++
-apt-get install -y libc++-dev libc++abi-dev
-
+update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-7 150 --slave /usr/bin/git-clang-format git-clang-format /usr/bin/clang-format-7
+update-alternatives --auto clang-format
+ 
+update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-7 150
+update-alternatives --auto lldb
 
 # Set clang as default compiler
-update-alternatives --install /usr/bin/cc cc /usr/bin/clang-6.0 100
-update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-6.0 100
-update-alternatives --set c++ /usr/bin/clang++-libc++ 
-
+update-alternatives --install /usr/bin/cc cc /usr/bin/clang-7 100
+update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-7 100
+update-alternatives --auto c++
 
 # Install LLVM linker lld and set it as default linker
-apt-get install -y lld
+#apt-get install -y lld
 update-alternatives --install /usr/bin/ld ld /usr/bin/x86_64-linux-gnu-ld 20
-update-alternatives --install /usr/bin/ld ld /usr/bin/lld 100
-
+update-alternatives --install /usr/bin/ld ld /usr/bin/lld-7 100
+update-alternatives --auto ld
 
 # Set the link archiver to the LLVM version
 update-alternatives --install /usr/bin/ar ar /usr/bin/x86_64-linux-gnu-ar 20
-update-alternatives --install /usr/bin/ar ar /usr/bin/llvm-ar-4.0 100
-
+update-alternatives --install /usr/bin/ar ar /usr/bin/llvm-ar-7 100
+update-alternatives --auto ar
 
 # Install cmake
-apt-get install -y cmake
+pip install cmake
 
 # Install coco-cpp
 apt-get install -y coco-cpp
